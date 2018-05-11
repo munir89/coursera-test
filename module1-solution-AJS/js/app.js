1ada7979919e832d170ff386b95b674a6d8d3ffb
Munir Salem <munir.salem89@gmail.com>
2018-05-11 16:06:28 +0200

    finish solution

---
+++ module1-solution-AJS/js/app.js
@@ -0,0 +1,36 @@
+(function () {
+
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', lcController);
+lcController.$injector = ['$scope'];
+
+function lcController ($scope) {
+  $scope.lunchMenu = '';
+  $scope.massage = '';
+  $scope.color = '';
+  
+  $scope.checkLunchMenue = function (){
+    var checker = '';
+    if($scope.lunchMenu === '') {
+      checker = 'Please enter data first';
+    $scope.color = 'red';
+    } else {
+      checker = check();
+    }
+    $scope.massage = checker;
+  };
+
+  function check () {
+    var msg = '';
+     if($scope.lunchMenu.length <= 3){
+      msg = 'Enjoy!';
+    } else {
+      msg = 'Too much!';
+    }
+    $scope.color = "green";
+    return msg;
+  }
+}
+})();
